2c8fd4bc50992523f902d8271e988ba2afe757fe
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-27 19:17:32 -0300

    Add materia ui and styled components

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,16 +4,21 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
-import { MainPage } from './pages/main'
-import { Contact } from './pages/contact'
+import { MainPage } from 'pages/main'
+import { Contact } from 'pages/contact'
+import { CssBaseline } from '@material-ui/core'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path='/' component={MainPage} exact />
-      <Route path='/contact' component={Contact} />
-    </Switch>
-  </BrowserRouter>
+  <>
+    <CssBaseline>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={MainPage} exact />
+          <Route path='/contact' component={Contact} />
+        </Switch>
+      </BrowserRouter>
+    </CssBaseline>
+  </>
 )
 
 export default App
