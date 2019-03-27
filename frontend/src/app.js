1faa55c37e71ddbca881f34b5da623e49dbee49b
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-27 18:51:22 -0300

    Add router for navigate between the pages

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom'
+import { MainPage } from './pages/main'
+import { Contact } from './pages/contact'
 
-const App = () => <div>oi</div>
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={MainPage} exact />
+      <Route path='/contact' component={Contact} />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App
