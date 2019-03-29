c9e17c8a5a4a22a24a3603f5b14aa1ee58a1f00c
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-29 13:15:36 -0300

    Refactor no main component

--- frontend/src/pages/main/main.js
+++ frontend/src/pages/main/main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import Card from '@material-ui/core/Card'
@@ -6,7 +6,7 @@ import Card from '@material-ui/core/Card'
 import charlieBrow from './charlie-brown.png'
 import cpm22 from './cpm22.png'
 import skank from './skank.png'
-import imageLeft from './imageleft.png'
+import offspring from './offspring.png'
 import projota from './projota.png'
 import sepultura from './sepultura.png'
 import slipknot from './slipknot.png'
@@ -17,189 +17,228 @@ import orappa from './orappa.png'
 import GridContainer from '../../common/grid-container'
 import GridContainerWithAnImage from '../../common/grid-container-with-an-image'
 import Grid from '@material-ui/core/Grid'
+import PropTypes from 'prop-types'
 
 import api from '../../services/api'
+
+import {
+  withStyles
+} from '@material-ui/core/styles'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: theme.spacing.unit * 2
+  }
+})
 
 const Container = styled.div`
   padding-top: 70px;
 `
+class Main extends Component {
+  state = {
+    posts: []
+  }
 
-// const initialState = {
-//   posts: []
-// }
+  async componentDidMount () {
+    const response = await api.get('posts')
 
-function Main (classes) {
-  // const posts = useState([])
+    this.setState({
+      posts: response.data
+    })
+  }
 
-  useEffect(() => {
-    api.get('http://localhost:81/v1/posts')
-      .then(response => console.log(response))
-  })
+  render () {
+    const { classes } = this.props
+    const { posts } = this.state
 
-  return (
-    <Container>
-      <Grid container direction='row' style={{ paddingBottom: '70px' }}>
-        <Grid item xs={6}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainer
-                image={imageLeft}
-                title='Kelsi Monahan'
-                subtitle1='Qui occaecati vero et quibusdam non'
-                subtitle2='Saepe quia culpa vero. Velit numquam corporis nihil sint enim exercitationem. Rem nulla illum sint et id dolore voluptas '
-              />
-            </CardActionArea>
-          </Card>
+    return (
+      <Container>
+        <Grid container direction='row' style={{ paddingBottom: '70px' }}>
+          {
+            posts.map(response => {
+              const id = parseInt(response.id)
+              if (id === 1 || id === 2) {
+                return (
+                  <Grid item xs={6}>
+                    <Card className={classes.card}>
+                      <CardActionArea>
+                        <GridContainer
+                          image={response.photo === 'offspring' ? offspring : projota}
+                          title={response.author}
+                          subtitle1={response.title}
+                          subtitle2={response.content}
+                        />
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                )
+              }
+            })
+          }
         </Grid>
 
-        <Grid item xs={6}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainer
-                image={projota}
-                title='Mrs. Alexanne Schneider'
-                subtitle1='Architecto quos rem unde quia accusantium'
-                subtitle2='Voluptatum omnis et dolor architecto non totam aspernatur sapiente. Et accusantium rem. Assumenda quia error'
-              />
-            </CardActionArea>
-          </Card>
-        </Grid>
-      </Grid>
+        <Grid container direction='row-reverse' style={{ paddingBottom: '70px' }}>
+          {
+            posts.map(response => {
+              const id = parseInt(response.id)
 
-      <Grid container direction='row-reverse' style={{ paddingBottom: '70px' }}>
-        <Grid item xs={8}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainerWithAnImage
-                image={charlieBrow}
-                title='Dagmar Ankunding'
-                subtitle1='Repellat aliquam quo sit qui praesentium ut aliquid'
-                subtitle2='Ea ut et labore quidem non sit in quidem. Provident est incidunt dignissimos enim qui consequatur numquam.'
-              />
-            </CardActionArea>
-          </Card>
-        </Grid>
-      </Grid>
-
-      <Grid container direction='row' style={{ paddingBottom: '70px' }}>
-        <Grid item xs={6}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainer
-                image={cpm22}
-                imageRightPosition
-                title='Kelsi Monahan'
-                subtitle1='Qui occaecati vero et quibusdam non'
-                subtitle2='Saepe quia culpa vero.'
-              />
-            </CardActionArea>
-          </Card>
+              if (id === 3) {
+                return (
+                  <Grid item xs={8}>
+                    <Card className={classes.card}>
+                      <CardActionArea>
+                        <GridContainerWithAnImage
+                          image={charlieBrow}
+                          title={response.author}
+                          subtitle1={response.title}
+                          subtitle2={response.content}
+                        />
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                )
+              }
+            })
+          }
         </Grid>
 
-        <Grid item xs={6} style={{ paddingBottom: '70px' }}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainer
-                image={skank}
-                imageRightPosition
-                title='Mrs. Alexanne Schneider'
-                subtitle1='Architecto quos rem unde quia accusantium'
-                subtitle2='Voluptatum omnis et dolor architecto non totam aspernatur sapiente. Et accusantium rem. Assumenda quia error'
-              />
-            </CardActionArea>
-          </Card>
-        </Grid>
-      </Grid>
-
-      <Grid container direction='row' style={{ paddingBottom: '70px' }}>
-        <Grid item xs={8}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainerWithAnImage
-                image={sepultura}
-                title='Sydney Legros'
-                subtitle1='Possimus saepe et illum molestiae et quibusdam'
-                subtitle2='Excepturi quod aliquid impedit earum consequatur eos. In quo fuga ad odio beatae'
-              />
-            </CardActionArea>
-          </Card>
-        </Grid>
-      </Grid>
-
-      <Grid container direction='row' style={{ paddingBottom: '70px' }}>
-        <Grid item xs={6}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainer
-                image={projota}
-                title='Kelsi Monahan'
-                subtitle1='Qui occaecati vero et quibusdam non'
-                subtitle2='Saepe quia culpa vero.'
-              />
-            </CardActionArea>
-          </Card>
+        <Grid container direction='row' style={{ paddingBottom: '70px' }}>
+          {
+            posts.map(response => {
+              const id = parseInt(response.id)
+              if (id === 4 || id === 5) {
+                return (
+                  <Grid item xs={6}>
+                    <Card className={classes.card}>
+                      <CardActionArea>
+                        <GridContainer
+                          image={response.photo === 'cpm22' ? cpm22 : skank}
+                          imageRightPosition
+                          title={response.author}
+                          subtitle1={response.title}
+                          subtitle2={response.content}
+                        />
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                )
+              }
+            })
+          }
         </Grid>
 
-        <Grid item xs={6}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainer
-                image={slipknot}
-                title='Mrs. Alexanne Schneider'
-                subtitle1='Architecto quos rem unde quia accusantium'
-                subtitle2='Voluptatum'
-              />
-            </CardActionArea>
-          </Card>
-        </Grid>
-      </Grid>
+        <Grid container direction='row' style={{ paddingBottom: '70px' }}>
+          {
+            posts.map(response => {
+              const id = parseInt(response.id)
 
-      <Grid container direction='row-reverse' style={{ paddingBottom: '70px' }}>
-        <Grid item xs={8}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainerWithAnImage
-                image={disturbed}
-                title='Sydney Legros'
-                subtitle1='Possimus saepe et illum molestiae et quibusdam'
-                subtitle2='Excepturi quod aliquid impedit earum consequatur eos. In quo fuga ad odio beatae'
-              />
-            </CardActionArea>
-          </Card>
-        </Grid>
-      </Grid>
-
-      <Grid container direction='row'>
-        <Grid item xs={6}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainer
-                image={tavares}
-                imageRightPosition
-                title='Kelsi Monahan'
-                subtitle1='Qui occaecati vero et quibusdam non'
-                subtitle2='Saepe quia culpa vero. Velit numquam corporis nihil sint enim exercitationem. Rem nulla illum sint et id dolore voluptas'
-              />
-            </CardActionArea>
-          </Card>
+              if (id === 6) {
+                return (
+                  <Grid item xs={8}>
+                    <Card className={classes.card}>
+                      <CardActionArea>
+                        <GridContainerWithAnImage
+                          image={sepultura}
+                          title={response.author}
+                          subtitle1={response.title}
+                          subtitle2={response.content}
+                        />
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                )
+              }
+            })
+          }
         </Grid>
 
-        <Grid item xs={6}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <GridContainer
-                image={orappa}
-                imageRightPosition
-                title='Mrs. Alexanne Schneider'
-                subtitle1='Architecto quos rem unde quia accusantium'
-                subtitle2='Voluptatum omnis et dolor architecto non totam'
-              />
-            </CardActionArea>
-          </Card>
+        <Grid container direction='row' style={{ paddingBottom: '70px' }}>
+          {
+            posts.map(response => {
+              const id = parseInt(response.id)
+              if (id === 7 || id === 8) {
+                return (
+                  <Grid item xs={6}>
+                    <Card className={classes.card}>
+                      <CardActionArea>
+                        <GridContainer
+                          image={response.photo === 'projota' ? projota : slipknot}
+                          title={response.author}
+                          subtitle1={response.title}
+                          subtitle2={response.content}
+                        />
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                )
+              }
+            })
+          }
         </Grid>
-      </Grid>
-    </Container>
-  )
+
+        <Grid container direction='row-reverse' style={{ paddingBottom: '70px' }}>
+          {
+            posts.map(response => {
+              const id = parseInt(response.id)
+
+              if (id === 9) {
+                return (
+                  <Grid item xs={8}>
+                    <Card className={classes.card}>
+                      <CardActionArea>
+                        <GridContainerWithAnImage
+                          image={disturbed}
+                          title={response.author}
+                          subtitle1={response.title}
+                          subtitle2={response.content}
+                        />
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                )
+              }
+            })
+          }
+        </Grid>
+
+        <Grid container direction='row'>
+          {
+            posts.map(response => {
+              const id = parseInt(response.id)
+              if (id === 10 || id === 11) {
+                return (
+                  <Grid item xs={6}>
+                    <Card className={classes.card}>
+                      <CardActionArea>
+                        <GridContainer
+                          image={response.photo === 'tavares' ? tavares : orappa}
+                          imageRightPosition
+                          title={response.author}
+                          subtitle1={response.title}
+                          subtitle2={response.content}
+                        />
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                )
+              }
+            })
+          }
+        </Grid>
+      </Container>
+    )
+  }
 }
 
-export default Main
+Main.propTypes = {
+  classes: PropTypes.string
+}
+
+export default withStyles(styles)(Main)
